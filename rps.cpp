f7a4c8a2450db02212a73a769f145981b44d3082
James Park <73313470+Jvmes-Park@users.noreply.github.com>
2021-01-24 11:48:09 -0500

    Update rps.cpp

--- rps.cpp
+++ rps.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <vector>
+#include <time.h>
 #include <random>
+
 using namespace std;
+
+//Current problems in the code:
+//It is not randomly generated. For some reason. 
+//If you make a wrong mistake in input, it wont accept the next right one. Possible solution: Change the cases to if else and while. Put the while error catcher before the if and else if. 
 
 int main() {
 	int player_choice;
+	srand (time(NULL));
 	int computer_choice = rand() %3 +1;
 	vector<string>ending = {"\nBruh, you lost to a computer!\n", "You won! Congrats!\n", "Its a draw!\n"};
 	vector<string>choice = {"\nRock\n", "\nPaper\n", "\nScissors\n"};
@@ -26,34 +34,54 @@ int main() {
 		       "     +---------------+\n"
 		       "          Entry:\n";
 		cin >> player_choice;
-		switch(player_choice) {
-			case 1:
-				cout <<choice[0];
-				break;
-			case 2:
-				cout <<choice[1];
-				break;
-			case 3:
-				cout <<choice[2];
-				break;
-			case 0:	
-				loop = false;
-				break;
-			default:
-				while(player_choice != 1 || player_choice != 2 || player_choice != 3 || player_choice != 0) {
+		if (player_choice == 1) {
+			cout <<choice[0];
+		}
+		else if (player_choice == 2) {
+			cout <<choice[1];
+		}
+		else if (player_choice == 3) {
+			cout <<choice[2];
+		}
+		else if (player_choice == 0) {
+			loop = false;
+		}
+		else {
+			bool player_error = true;
+			while (player_error) {
+				while(player_choice != 1 && player_choice != 2 && player_choice != 3 && player_choice != 0) {
 					cout <<"  ERROR! INVALID INPUT, try again.\n";
 					cout <<"     +---------------+\n"
 					       "     |  Choices:     |\n"
 					       "     |               |\n"
 					       "     |  Rock(1)      |\n"
-					       "     |  Paper(2)     |\n"
+		 			       "     |  Paper(2)     |\n"
 					       "     |  Scissors(3)  |\n"
 					       "     |  Exit(0)      |\n"
 					       "     +---------------+\n"
 					       "        Entry:\n";
-
-					cin >> player_choice;
+					cin >>player_choice;
+					if (player_choice == 1) {
+						cout <<choice[0];
+						player_error = false;
+					}
+					else if (player_choice == 2) {
+						cout <<choice[1];
+						player_error = false;
+					}
+					else if (player_choice == 3) {
+						cout <<choice[2];
+						player_error = false;
+					}
+					else if (player_choice == 0) {
+						loop = false;
+						player_error = false;
+					}
+					else {
+	
+							}
 				}
+		}
 		}
 		cout << "V.S...\n"<<endl;
 		switch(computer_choice) {
@@ -81,34 +109,52 @@ int main() {
 			cout <<ending[1];
 		else
 			cout <<ending[0];
+		bool game_over = true;
+		while(game_over) {
 
-		cout <<"        +---------------+\n"
-		       "        |  Play Again?  |\n"
-		       "        |               |\n"
-		       "        |    Yes (1)    |\n"
-		       "        |    No  (0)    |\n"
-		       "        +---------------+\n";
-		char restart;
-		cin >>restart;
-		switch(restart) {
-			case 1:
-				//Nothing happens because its already in a play loop//
-			case 0:
+			cout <<"        +---------------+\n"
+			       "        |  Play Again?  |\n"
+			       "        |               |\n"
+			       "        |    Yes (1)    |\n"
+			       "        |    No  (0)    |\n"
+			       "        +---------------+\n";
+			int restart;
+			cin >>restart;
+			if (restart == 1 ){
+				game_over = false;
+			;}
+			else if (restart == 0) {
 				loop = false;
-				break;
-			default:
-				while (restart != 1 || restart != 0) {
-				cout <<"  ERROR! INVALID INPUT, try again.\n";
-				cout <<"        +---------------+\n"
-				       "        |  Play Again?  |\n"
-				       "        |               |\n"
-				       "        |    Yes (1)    |\n"
-				       "        |    No  (0)    |\n"
-				       "        +---------------+\n";
-					cin >>restart;
-				}
+				game_over = false;
+			}
+			else	{
+				bool is_error = true;
+				while(is_error) {
+					while (restart != 1 && restart != 0) {
+						cout <<"  ERROR! INVALID INPUT, try again.\n";
+						cout <<"        +---------------+\n"
+						       "        |  Play Again?  |\n"
+						       "        |               |\n"
+						       "        |    Yes (1)    |\n"
+						       "        |    No  (0)    |\n"
+						       "        +---------------+\n";
+							cin >>restart;
+							if (restart == 1) {
+								is_error = false;
+								game_over = false;
+							}
+							else if (restart == 2) {
+								is_error = false;
+								loop = false;
+								game_over = false;
+							}
+								else {
+								;
+							}
+					}
+			}
 		}
-
+	}
 	}
 	return 0;
 }
